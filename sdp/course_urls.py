089738ea1d8ed9f7d3c30b767e6e9b7bbcdab1a0
Charlie Chen <Charliechen_1@163.com>
2016-12-01 17:19:47 +0800

    Admin and HR added into the urls

--- sdp/course_urls.py
+++ sdp/course_urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import include, url
 from . import course_views
 
 urlpatterns = [
-    url(r'^(?P<identity>Participant|Instructor)/(?P<username>[A-Za-z\d_\s]+)', course_views.view_course, name='view_course'),
+    url(r'^(?P<identity>Participant|Instructor|HR|Administrator)/(?P<username>[A-Za-z\d_\s]+)', course_views.view_course, name='view_course'),
     url(r'^create_module/(?P<username>[A-Za-z\d_\s]+)', course_views.create_module, name='create_module'),
     url(r'^(?P<module>[A-Za-z\d_\s]+)/', include('sdp.module_urls')),
 
