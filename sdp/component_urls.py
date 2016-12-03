45fdffe75b0c2b3fb48f7305c152f5ace3cb2347
Sun Peigen <sunbacon@Suns-MacBook-Pro-2.local>
2016-12-03 11:24:54 +0800

    all finished

--- sdp/component_urls.py
+++ sdp/component_urls.py
@@ -9,6 +9,8 @@ urlpatterns = [
     url(r'^File/(?P<identity>Participant|Instructor)/(?P<username>[A-Za-z\d_\s]+)', component_views.view_file, name='view_component_file'),
     url(r'^File/(?P<identity>Participant|Instructor)/(?P<username>[A-Za-z\d_\s]+)', component_views.view_file, name='view_component_video'),
 =======
+    # url(r'^modify_component/(?P<username>[A-Za-z\d_\s]+)', component_views.modify_component, name='modify_component'),
+    url(r'^Text/(?P<identity>Participant|Instructor)/(?P<username>[A-Za-z\d_\s]+)', component_views.view_text, name='view_component_text'),
     url(r'^Text/(?P<identity>Participant|Instructor|HR|Administrator)/(?P<username>[A-Za-z\d_\s]+)', component_views.view_text, name='view_component_text'),
     url(r'^Image/(?P<identity>Participant|Instructor|HR|Administrator)/(?P<username>[A-Za-z\d_\s]+)', component_views.view_image, name='view_component_image'),
     url(r'^File/(?P<identity>Participant|Instructor|HR|Administrator)/(?P<username>[A-Za-z\d_\s]+)', component_views.view_file, name='view_component_file'),
